f0030b9dbf7f2a652aaa2b4d84127130c37f8972
Emilia Rodriguez Gimenez <emiliamartin97@gmail.com>
2020-04-09 19:28:40 -0300

    fix fetch failure

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ const forecast = require('./utils/forecast');
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
 hbs.registerPartials(partialsPath)
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../public')))
 
