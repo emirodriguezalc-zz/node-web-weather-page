0cc34ff64f3d340aa8f943b1e5855b35031b37a9
Emilia Rodriguez Gimenez <emiliamartin97@gmail.com>
2020-04-09 17:09:11 -0300

    add error handling condition

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit', (event) => {
 
   fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
-      if (data.error) {
+      if (data.error || !data) {
         messageTwo.textContent = '';
         messageOne.textContent = 'There has been an error';
       } else {
